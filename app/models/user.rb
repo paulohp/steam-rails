f6483e7e3c2f888fb22e0dc87842acb8caf28d8f
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-08-05 18:14:24 -0300

    added more infos in create_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   attr_accessor :login
-  attr_accessible :login
+
   validates :username,
     :uniqueness => {
       :case_sensitive => false
@@ -14,10 +14,12 @@ class User < ActiveRecord::Base
 
   def self.find_for_steam_oauth(access_token, signed_in_resource=nil)
     data = access_token.info
+    provider = access_token.provider
+    uid = access_token.uid
     if user = User.where(:username => data["nickname"]).first
       user
     else
-      User.create!(:username => data["nickname"] ,:email => "example@company.com", :password => Devise.friendly_token[0,20])
+      User.create!(:provider => provider, :uid => uid, :picture => data['image'] :username => data["nickname"] ,:email => "example@company.com", :password => Devise.friendly_token[0,20])
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130805201139) do
+ActiveRecord::Schema.define(version: 20130805211227) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20130805201139) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "username"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
